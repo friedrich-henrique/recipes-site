73778d510376189af62e36592a6a577acc3ee710
oqfs06 <oqfs06@mailbox.unideb.hu>
2022-08-13 11:17:59 +0200

    changes on title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,9 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            Daiane's recipe website
+            <h2>
+                Daiane's recipe website
+            </h2>
         </div>
     </body>
 </html>
